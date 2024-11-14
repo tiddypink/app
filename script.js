b1cac0df2a47da8988102de79e11fdfeb4918c79
tiddypink <minato.x3ck@gmail.com>
2024-11-14 16:27:50 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
   if(!isLocal){
     const img = document.getElementById('image');
     img.onload = function() {
-      image.onload = null;
+      img.onload = null;
               const canvas = document.createElement('canvas');
               const ctx = canvas.getContext('2d');
               canvas.width = img.width;
